7cc3459aef596abb51b49c427f96798309506ab6
Atrypk <atrypk@gmail.com>
2017-06-21 21:51:46 +0200

    Implementados metodos de la API (Sin conexión a BD aún, solo estructura y funcionalidad envio/recepcion de parametros)

--- restapi/gamma/index.php
+++ restapi/gamma/index.php
@@ -10,16 +10,69 @@ header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"'); //Acceso a la
 
 //Includes
 //include_once '../include/llavesBD.php';
-
-
 require '../../vendor/autoload.php';
 
+//Configuracion para evitar errores de headerLength
 $config = ['settings' => [
     'addContentLengthHeader' => false,
 ]];
+
+//Creacion de objeto app
 $app = new Slim\App($config);
-$app->get('/', function () {
-   echo "Funciona :)";
+
+//Llamadas
+
+//Crear nota
+$app->post('/crearNota', function ($request, $response, $args) {
+
+	//Recogemos el contenido enviado
+    $json = $request->getBody();
+
+    //Pasamos el json a array
+    $data = json_decode($json, true);
+
+    //creamos el objeto nota
+    $objNota = (object)$data;
+
+    //Mostramos
+    echo $objNota->name;
+ 
+});
+
+//Consultar todas las notas
+$app->get('/consultarNotas', function ($request, $response, $args) {
+
+	
+    //Mostramos
+    echo "Mostrando todas las notas";
+ 
+});
+
+//Consultar una nota concreta
+$app->get('/consultarNota/{idNota}', function ($request, $response, $args) {
+
+	
+    //Mostramos
+    echo "Mostrando nota " . $args["idNota"] ;
+ 
+});
+
+//Marcar nota como favorita
+$app->put('/marcarFavorita/{idNota}', function ($request, $response, $args) {
+
+	
+    //Mostramos
+    echo "Marcando como favorita la nota " . $args["idNota"] ;
+ 
+});
+
+//Consultar todas las notas favoritas
+$app->get('/consultarNotasFavoritas', function ($request, $response, $args) {
+
+	
+    //Mostramos
+    echo "Mostrando todas las notas favoritas";
+ 
 });
 
 $app->run();
